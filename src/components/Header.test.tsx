42e755b620bb3bf4acf0bb7331b79862e8ba4fc3
jtannas <jtannas@gmail.com>
2026-02-12 16:00:30 -0700

    appearse `tsc`

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { expect, test, vi } from "vitest";
+import { expect, test } from "vitest";
 import MyComponent from "./Header";
 
 test("Can use Testing Library", () => {
