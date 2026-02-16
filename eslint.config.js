bdd1c088ad7c6c35add4b06f3ea1282411f0552d
jtannas <jtannas@gmail.com>
2026-02-16 13:47:15 -0700

    fix missing eslint config setting

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ import tsPlugin from 'typescript-eslint';
 
 export default defineConfig([
   {
+		files: ["**/*.ts", "**/*.tsx"],
 		ignores: [
 			"**/convex/_generated/**",
 			"**/src/routeTree.gen.ts",
