a9335f8c95a61f5af206d52ebcf7498c4487f9fa
jtannas <jtannas@gmail.com>
2026-02-16 15:41:16 -0700

    `bun run format --fix`

--- convex/auth.ts
+++ convex/auth.ts
@@ -2,7 +2,7 @@ import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
 import { Password } from "@convex-dev/auth/providers/Password";
 import { convexAuth } from "@convex-dev/auth/server";
 
-export const { auth,     } = convexAuth({
+export const { auth } = convexAuth({
 	providers: [
 		Password({
 			profile(params) {
