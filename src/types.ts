eb7539b80312b78295953172e022a23f687fdf7f
Joel Tannas <jtannas@gmail.com>
2026-02-21 22:28:29 -0700

    Apply suggestion from @coderabbitai[bot]
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 // Primarily for utility types that are applicable even in static assets
 
-export type DeepReadonly<T> = T extends object
-	? { readonly [K in keyof T]: DeepReadonly<T[K]> }
-	: T extends Array<infer U>
-		? ReadonlyArray<DeepReadonly<U>>
+export type DeepReadonly<T> = T extends Array<infer U>
+	? ReadonlyArray<DeepReadonly<U>>
+	: T extends object
+		? { readonly [K in keyof T]: DeepReadonly<T[K]> }
 		: T;
