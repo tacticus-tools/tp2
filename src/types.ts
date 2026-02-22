d825da6312cf5cfe051f8bba098917f623973ffd
jtannas <jtannas@gmail.com>
2026-02-21 22:31:26 -0700

    `bun fix`

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,8 @@
 // Primarily for utility types that are applicable even in static assets
 
-export type DeepReadonly<T> = T extends Array<infer U>
-	? ReadonlyArray<DeepReadonly<U>>
-	: T extends object
-		? { readonly [K in keyof T]: DeepReadonly<T[K]> }
-		: T;
+export type DeepReadonly<T> =
+	T extends Array<infer U>
+		? ReadonlyArray<DeepReadonly<U>>
+		: T extends object
+			? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+			: T;
